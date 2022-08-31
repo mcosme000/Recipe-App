b5d15e114232e25ec38340cd22f33960ce83481e
mcosme000 <mcosme00@gmail.com>
2022-08-31 17:39:38 +0900

    Added styles and labels with meal type data from the API

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const apiKey = "cd04877bbc5e2a254448c3464867d456";
 
 // GETTING ELEMENTS FROM HTML //
 let input = document.getElementById("input");
+let showTitle = document.getElementById("show-title");
 const submit = document.getElementById("submit");
 const container = document.getElementById("container");
 const cardContainer = document.getElementById("card-container");
@@ -14,8 +15,10 @@ const cardContainer = document.getElementById("card-container");
 submit.addEventListener("click", (e) => {
   e.preventDefault();
   let inputValue = input.value;
+  showTitle.innerHTML = inputValue;
   input.value = "";
   startApi(inputValue);
+  cleanContainer();
 });
 
 const createRecipeCard = (data) => {
@@ -24,24 +27,35 @@ const createRecipeCard = (data) => {
   for (let i = 0; i < data.length; i++) {
     //GET VARIABLES FROM DATA//
     let recipeTitle = data[i].recipe.label;
+    let healthLabels = data[i].recipe.healthLabels;
+    let mealType = data[i].recipe.mealType;
     imgAtt = data[i].recipe.image;
     //CREATE ELEMENTS//
     let cardElement = document.createElement("DIV");
     let imgContainer = document.createElement("DIV");
+    let labelContainer = document.createElement("DIV");
+    labelContainer.classList.add("label-container");
+    for (let j = 0; j < mealType.length; j++) {
+      let label = document.createElement("P");
+      label.classList.add("label");
+      label.innerHTML = mealType[j];
+      console.log(label);
+      labelContainer.appendChild(label);
+    }
     let imgElement = document.createElement("IMG");
     let recipeContentContainer = document.createElement("DIV");
     let titleElement = document.createElement("H3");
-    let descriptionElement = document.createElement("P");
 
     //CHANGE VALUES OF ELEMENTS//
     cardElement.classList.add("card");
+    recipeContentContainer.classList.add("recipe-content");
     titleElement.innerHTML = recipeTitle;
     imgElement.setAttribute("src", imgAtt);
 
     //APPEND ELEMENTS//
     imgContainer.appendChild(imgElement);
     recipeContentContainer.appendChild(titleElement);
-    recipeContentContainer.appendChild(descriptionElement);
+    recipeContentContainer.appendChild(labelContainer);
     cardElement.appendChild(imgContainer);
     cardElement.appendChild(recipeContentContainer);
     cardContainer.appendChild(cardElement);
@@ -65,4 +79,11 @@ const getRecipes = (data) => {
 const startApi = (input) => {
   let searchRecipes = `https://api.edamam.com/api/recipes/v2?type=public&q=${input}&app_id=${appId}&app_key=${apiKey}`;
   axios.get(searchRecipes).then(getRecipes);
+};
+
+const cleanContainer = () => {
+  console.log("Cleaning container...");
+  while (cardContainer.firstChild) {
+    cardContainer.removeChild(cardContainer.firstChild);
+  }
 };
