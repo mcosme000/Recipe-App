24b30b3c882a9f75e60976e3500462b03e160d46
Maria Cosmen <mcosme00@gmail.com>
2023-08-01 20:09:15 +0900

    Fix icons names

--- src/js/dietary_icons.js
+++ src/js/dietary_icons.js
@@ -1,11 +1,13 @@
 export const dietary_icons = (data) => {
-  const iconHealth = ["Vegan", "Gluten-Free", "Dairy-Free"];
+  const iconHealth = ["vegan", "gluten-gree", "dairy-free"];
   let logoContainer = document.createElement("DIV")
   data.forEach((n) => {
     iconHealth.forEach((icon) => {
-      if (icon == n) {
+      if (icon == n.toLowerCase()) {
+        console.log(n.toLowerCase());
         let logo = document.createElement("img");
-        logo.setAttribute("src", `media/${n}.png`);
+        logo.setAttribute("src", `media/${n.toLowerCase()}.png`);
+        console.log(logo);
         logo.classList.add("icon")
         logoContainer.appendChild(logo);
       }
